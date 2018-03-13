50ace36034c6566bfb21ab7c283d0606cf9d0ddf
Jannis Linxweiler <linxweiler@gmx.de>
2018-03-13 14:54:25 +0100

    - minimal example using GLFW

--- application/InteractiveGlut/colorMap.cu
+++
@@ -1,6 +0,0 @@
-#include "colorMap.cuh"
-#include <cuda_runtime.h>
-
-__constant__ float colorMapDeviceR[36];
-__constant__ float colorMapDeviceG[36];
-__constant__ float colorMapDeviceB[36];
---
+++ application/tutorial/3rdPartyLinking.cmake
@@ -0,0 +1,21 @@
+include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Cuda/Link.cmake)
+linkCuda()
+
+include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/OpenGL/Link.cmake)
+linkOpenGL(${targetName})
+
+if(APPLE)
+    #link_directories(${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib)
+    #link_directories(${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib ${CUDA_CUT_INCLUDE_DIR}/../lib/darwin/)
+    #find_library(GLUT_LIBRARY GLUT)
+
+    find_library(COCOA_LIBRARY COCOA)
+    find_library(QuartzCore_LIBRARY QuartzCore)
+    find_library(IOKit_LIBRARY IOKit)
+    
+    #CMake requires absolut pathes here for libraries that a not in LD_Library_PATH.
+    #TODO: create marcros for:
+    # GLFW (available in source code with CMake build files)
+    # GLEW is included here
+    target_link_libraries(${targetName} "${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib/libglfw3.a" "${COCOA_LIBRARY}" "${QuartzCore_LIBRARY}" "${IOKit_LIBRARY}")
+endif()
