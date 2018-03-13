68b878c839d5f40ad5f2cd6cba27f5ea7cb02936
LEGOLAS\lenz <lenz@irmb.tu-bs.de>
2018-03-13 16:45:26 +0100

    refactoring of glfw linking

--- application/tutorial/3rdPartyLinking.cmake
+++ application/tutorial/3rdPartyLinking.cmake
@@ -4,6 +4,8 @@ linkCuda()
 include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/OpenGL/Link.cmake)
 linkOpenGL(${targetName})
 
+    
+
 if(APPLE)
     #link_directories(${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib)
     #link_directories(${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib ${CUDA_CUT_INCLUDE_DIR}/../lib/darwin/)
@@ -12,12 +14,12 @@ if(APPLE)
     find_library(COCOA_LIBRARY COCOA)
     find_library(QuartzCore_LIBRARY QuartzCore)
     find_library(IOKit_LIBRARY IOKit)
+    target_link_libraries(${targetName} "${COCOA_LIBRARY}" "${QuartzCore_LIBRARY}" "${IOKit_LIBRARY}")
     
     #CMake requires absolut pathes here for libraries that a not in LD_Library_PATH.
     #TODO: create marcros for:
     # GLFW (available in source code with CMake build files)
-    # GLEW is included here
-    target_link_libraries(${targetName} "${CMAKE_SOURCE_DIR}/3rdparty/glfw/lib/darwin/libglfw3.a" "${COCOA_LIBRARY}" "${QuartzCore_LIBRARY}" "${IOKit_LIBRARY}")
+    target_link_libraries(${targetName} "${GLFW_BIN_PATH}/libglfw3.a")
 else()
-    target_link_libraries(${targetName} "${glfw_path}/build/src/Release/glfw3.lib")
+    target_link_libraries(${targetName} "${GLFW_BIN_PATH}/glfw3.lib")
 endif()
